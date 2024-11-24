2c4d119d174e1b0c55afb3f09cf7e1a4d2ae02e4
matheeshaw25 <matheeshawijerathna@gmail.com>
2024-11-24 22:53:25 +0530

    Big fix for Update Task

--- main.py
+++ main.py
@@ -36,10 +36,10 @@ def read_task(task_id: UUID):
     return HTTPException(status_code=404, detail="Task not found") #if task not there
 
 @app.put("/tasks/{task_id}", response_model=Task)
-def update_task(task_id : UUID , task_update : Task):
+def update_task(task_id: UUID, task_update: Task):
     for idx, task in enumerate(tasks):
         if task.id == task_id:
-            updated_task = task.copy(updated = task_update.dict(exclude_unset=True))
+            updated_task = task.copy(update=task_update.dict(exclude_unset=True))
             tasks[idx] = updated_task
             return updated_task
         
