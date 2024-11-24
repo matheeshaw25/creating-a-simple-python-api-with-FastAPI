5d6ea6cb73649cd63362a3ef6329d003db381121
matheeshaw25 <matheeshawijerathna@gmail.com>
2024-11-24 22:41:14 +0530

    Add PUT and DELETE

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 from uuid import UUID, uuid4 # unique identifier
@@ -25,6 +25,35 @@ def create_task(task: Task): # create new task using Pydantic model
 @app.get("/tasks/", response_model=List[Task]) # 
 def read_tasks():
     return tasks
+
+
+@app.get("/tasks/{task_id}", response_model=Task) 
+def read_task(task_id: UUID):
+    for task in tasks: #check each task in list with id passed
+        if task.id == task_id:
+            return task
+    
+    return HTTPException(status_code=404, detail="Task not found") #if task not there
+
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id : UUID , task_update : Task):
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            updated_task = task.copy(updated = task_update.dict(exclude_unset=True))
+            tasks[idx] = updated_task
+            return updated_task
+        
+    raise HTTPException(status_code=404, detail="Task not found")
+
+
+@app.delete("/tasks/{task_id}", response_model= Task)
+def delete_task(task_id : UUID):
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            deleted_task = tasks.pop(idx)
+            return deleted_task
+        
+    raise HTTPException(status_code=404, detail="Task not found")
 
 
 # Run the API
